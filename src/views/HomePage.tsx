a5c153a15e3497dd86a63448ac08efb9e669d224
Ophelie Bugnon <ophelie.bugnon@edu.hefr.ch>
2024-02-20 23:09:21 +0100

    [FEAT]: Initializing - Created directories - Created and displayed first component

---
+++ src/views/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div className="App">
+            <h1>HelloWorld</h1>
+            <h1>Trust the process</h1>
+        </div>
+    );
+}
+
+export default HomePage;
